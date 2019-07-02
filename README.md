813d8b46d9a826d8bf9e88008d4773b314cc1190
Eric Londaits <eric.londaits@gmail.com>
2019-07-02 16:26:15 -0300

    Documents installation and the build process.

--- README.md
+++ README.md
@@ -6,6 +6,33 @@ This repository builds the book.
 
 ## Installing
 
+- Ideally create a virtual environment (virtualenv) with python 3.7
+
+- Activate the new virtual environment
+
+- Install the dependencies by running
+
+```
+pip install -r requirements.txt
+```
+
 ## Building
 
+The build process uses the `invoke` task runner, which is installed as a dependency. 
 
+Run
+
+```
+invoke build
+```
+
+(assuming `invoke` is in your path. It'll be installed in the `bin` directory of your virtual env).
+
+### Other tasks
+
+There are other tasks you can `invoke`:
+
+- `build_state_images`: Builds the SVG files that represent game states
+- `build_html`: Builds the HTML file
+- `build_pdf`: Converts the HTML (plus SVG) to a PDF file
+- `clean`: Deletes output SVG, HTML and PDF file
--- tasks.py
+++ tasks.py
@@ -1,11 +1,13 @@
 import os
 from invoke import task
 
+
 @task
 def build_state_images(c):
     outdir = os.path.abspath(os.path.relpath('build/html/img/states', os.path.dirname(__file__)))
     print(f'Building state images to {outdir}')
     c.run(f'env/bin/python3 src/build-state-images.py {outdir}')
+
 
 @task
 def build_html(c):
@@ -13,27 +15,22 @@ def build_html(c):
     print('Building HTML')
     c.run(f'env/bin/python3 src/build-html.py {outpath}')
 
-
 @task
 def build_pdf(c):
     infile = os.path.abspath(os.path.relpath('build/html/index.html', os.path.dirname(__file__)))
     outfile = os.path.abspath(os.path.relpath('build/nim.pdf', os.path.dirname(__file__)))
     c.run(f'env/bin/weasyprint {infile} {outfile}')
 
-@task(build_state_images, build_html)
-def build(c, state_images=False):
-    c.run('env/bin/python3 --version')
+@task(build_state_images, build_html, build_pdf, default=True)
+def build(c):
+    pass
 
 @task
-def path(c):
-    print(os.path.dirname(__file__))
-
-@task
-def clean(c, html=False, state_images=False):
+def clean(c, html=True, state_images=True):
     patterns = ['build/nim.pdf']
     if html:
         patterns.append('build/html/index.html')
     if state_images:
-        patterns.append('build/html/img/states')
+        patterns.append('build/html/img/states/*.svg')
     for pattern in patterns:
         c.run("rm -rf {}".format(pattern))
